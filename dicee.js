d163da657c12f10ac64cdc13d6fbfe8d43249c7f
samuelNe-Dev <negash.samuel@yahoo.de>
2020-10-18 20:21:51 +0200

    first commit

---
+++ dicee.js
@@ -0,0 +1,28 @@
+
+
+function rollDices(){
+    var randomNumber1 = Math.floor(Math.random()*6);
+    var randomNumber2 = Math.floor(Math.random()*6);
+    var images = ["/images/dice1.png", "/images/dice2.png", "/images/dice3.png",
+    "/images/dice4.png", "/images/dice5.png", "/images/dice6.png"];
+    document.querySelector(".img1").setAttribute("src", images[randomNumber1]);
+    document.querySelector(".img2").setAttribute("src", images[randomNumber2]);
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerText = "Player 1 wins!";
+    }   
+    else if(randomNumber1 === randomNumber2){
+        document.querySelector("h1").innerText = "Draw";
+    }   
+    else{
+        document.querySelector("h1").innerText = "Player 2 wins!";
+    }
+
+    setTimeout('refreshTitle()', 5000);
+}
+
+function refreshTitle(){
+    document.querySelector("h1").innerText = "Click me!";
+    
+}
+
